ea18a66de659007ac6202941f442e28699098d7d
adamcrow64 <adamcrow63@gmail.com>
2017-10-27 08:25:24 +1100

    Added Message wrapper for Rules

---
+++ src/main/java/life/genny/qwanda/message/QDataRuleMessage.java
@@ -0,0 +1,23 @@
+package life.genny.qwanda.message;
+
+import life.genny.qwanda.rule.Rule;
+
+public class QDataRuleMessage extends QDataMessage {
+
+  private static final long serialVersionUID = 1L;
+  private Rule[] items;
+  private static final String DATATYPE_RULE = Rule.class.getSimpleName();
+
+  public QDataRuleMessage(final Rule[] items) {
+    super(DATATYPE_RULE);
+    setItems(items);
+  }
+
+  public Rule[] getItems() {
+    return items;
+  }
+
+  public void setItems(final Rule[] items) {
+    this.items = items;
+  }
+}
--- src/main/java/life/genny/qwanda/rule/Rule.java
+++ src/main/java/life/genny/qwanda/rule/Rule.java
@@ -1,26 +1,20 @@
 /*
  * (C) Copyright 2017 GADA Technology (http://www.outcome-hub.com/) and others.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  *
- * Contributors:
- *     Adam Crow
- *     Byron Aguirre
+ * Contributors: Adam Crow Byron Aguirre
  */
 
 package life.genny.qwanda.rule;
-
-import java.io.Serializable;
 
 import javax.persistence.DiscriminatorColumn;
 import javax.persistence.DiscriminatorType;
@@ -32,15 +26,13 @@ import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
-
+import java.io.Serializable;
 import life.genny.qwanda.CodedEntity;
 
 /**
- * Rule represents a distinct abstract Rule
- * managed in the Qwanda library.
- * An rule may be used directly in processing meaning for an event
- * using inference and induction  This
- * rule information includes:
+ * Rule represents a distinct abstract Rule managed in the Qwanda library. An rule may be used
+ * directly in processing meaning for an event using inference and induction This rule information
+ * includes:
  * <ul>
  * <li>The Human Readable name for this rule (used for summary lists)
  * <li>The unique code for the rule
@@ -52,10 +44,10 @@ import life.genny.qwanda.CodedEntity;
  * <p>
  * 
  * 
- * @author      Adam Crow
- * @author      Byron Aguirre
- * @version     %I%, %G%
- * @since       1.0
+ * @author Adam Crow
+ * @author Byron Aguirre
+ * @version %I%, %G%
+ * @since 1.0
  */
 
 
@@ -67,73 +59,73 @@ import life.genny.qwanda.CodedEntity;
 
 
 public class Rule extends CodedEntity implements Serializable {
-	
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
 
-	private static final String DEFAULT_CODE_PREFIX = "RUL_";
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 1L;
 
-
-	@NotNull
-	@Size(min=0, max=2000)
-	public String rule;
-
-	
-	/**
-	  * Constructor.
-	  * 
-	  * @param none
-	  */
-
-	public Rule()
-	{
-		super();
-		// dummy for hibernate
-	}
-	
-// assume valid rule
-	public Rule(String aCode, String aName, String rule)
-	{
-		super(aCode, aName);
-		setRule(rule);
-	}
-	
-	
-
-	/**
-	 * @return the rule
-	 */
-	public String getRule() {
-		return rule;
-	}
+  private static final String DEFAULT_CODE_PREFIX = "RUL_";
 
 
-	/**
-	 * @param rule the rule to set
-	 */
-	public void setRule(String rule) {
-		this.rule = rule;
-	}
+  @NotNull
+  @Size(min = 0, max = 10000)
+  public String rule;
 
 
-	/**
-	 * getDefaultCodePrefix This method is overrides the Base class
-	 * 
-	 * @return the default Code prefix for this class.
-	 */
-	static public String getDefaultCodePrefix() {
-		return DEFAULT_CODE_PREFIX;
-	}
+  /**
+   * Constructor.
+   * 
+   * @param none
+   */
 
-	
-	/* (non-Javadoc)
-	 * @see java.lang.Object#toString()
-	 */
-	@Override
-	public String toString() {
-		return "Rule:"+getCode()+"(" + getName()+") ";
-	}
-	
+  public Rule() {
+    super();
+    // dummy for hibernate
+  }
+
+  // assume valid rule
+  public Rule(final String aCode, final String aName, final String rule) {
+    super(aCode, aName);
+    setRule(rule);
+  }
+
+
+
+  /**
+   * @return the rule
+   */
+  public String getRule() {
+    return rule;
+  }
+
+
+  /**
+   * @param rule the rule to set
+   */
+  public void setRule(final String rule) {
+    this.rule = rule;
+  }
+
+
+  /**
+   * getDefaultCodePrefix This method is overrides the Base class
+   * 
+   * @return the default Code prefix for this class.
+   */
+  static public String getDefaultCodePrefix() {
+    return DEFAULT_CODE_PREFIX;
+  }
+
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see java.lang.Object#toString()
+   */
+  @Override
+  public String toString() {
+    return "Rule:" + getCode() + "(" + getName() + ") ";
+  }
+
 }

19ba33a30e3f5152322c02c75d26a581563ac067
Adam Crow <acrow@Adams-MacBook-Pro.local>
2017-11-22 02:30:24 +1100

    Updated RuleMessage to have token

--- src/main/java/life/genny/qwanda/message/QDataRuleMessage.java
+++ src/main/java/life/genny/qwanda/message/QDataRuleMessage.java
@@ -7,6 +7,7 @@ public class QDataRuleMessage extends QDataMessage {
   private static final long serialVersionUID = 1L;
   private Rule[] items;
   private static final String DATATYPE_RULE = Rule.class.getSimpleName();
+  private String token;
 
   public QDataRuleMessage(final Rule[] items) {
     super(DATATYPE_RULE);
@@ -20,4 +21,20 @@ public class QDataRuleMessage extends QDataMessage {
   public void setItems(final Rule[] items) {
     this.items = items;
   }
+
+/**
+ * @return the token
+ */
+public String getToken() {
+	return token;
+}
+
+/**
+ * @param token the token to set
+ */
+public void setToken(String token) {
+	this.token = token;
+}
+  
+  
 }

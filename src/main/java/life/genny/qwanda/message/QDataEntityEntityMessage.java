b630718eaec5dac86d53c40654578cbbd1a23bf8
Adam Crow <acrow@Adams-MacBook-Pro.local>
2017-11-22 15:18:19 +1100

    Added new entityentity message

---
+++ src/main/java/life/genny/qwanda/message/QDataEntityEntityMessage.java
@@ -0,0 +1,48 @@
+package life.genny.qwanda.message;
+
+
+import life.genny.qwanda.entity.EntityEntity;
+
+public class QDataEntityEntityMessage extends QDataMessage {
+  private static final long serialVersionUID = 1L;
+  private EntityEntity[] items;
+  private static final String DATATYPE_ENTITYENTITY = EntityEntity.class.getSimpleName();
+  private boolean clearLinks = true;
+
+  public QDataEntityEntityMessage(final EntityEntity[] items, boolean clearLinks) {
+    super(DATATYPE_ENTITYENTITY);
+    setItems(items);
+  }
+
+  public QDataEntityEntityMessage(final EntityEntity[] items) {
+    this(items, true);
+  }
+
+
+
+  public EntityEntity[] getItems() {
+    return items;
+  }
+
+  public void setItems(final EntityEntity[] items) {
+    this.items = items;
+  }
+
+/**
+ * @return the clearLinks
+ */
+public boolean isClearLinks() {
+	return clearLinks;
+}
+
+/**
+ * @param clearLinks the clearLinks to set
+ */
+public void setClearLinks(boolean clearLinks) {
+	this.clearLinks = clearLinks;
+}
+
+  
+
+
+}

b579f8fab7dda0bd7355ed37b3faff02fdc4e4e4
rpgayatri <rpgayatri@gmail.com>
2017-11-28 14:24:00 +1100

    message model

--- src/main/java/life/genny/qwanda/message/QMSGMessage.java
+++ src/main/java/life/genny/qwanda/message/QMSGMessage.java
@@ -1,29 +1,78 @@
 package life.genny.qwanda.message;
 
-import java.util.List;
+import java.util.Arrays;
 
 public class QMSGMessage extends QMessage {
 	
 	private static final String MESSAGE_TYPE = "MSG_MESSAGE";
-	private List<QBaseMSGMessage> msgMessageData;
+	
+	private static final String event_type = "message";
+	
+	private String template_code;
+	private String[] msgMessageData;
+	private QBaseMSGMessageType msgMessageType;
+	private String[] attachments;
+	
+	//private List<QBaseMSGMessage> msgMessageData;
 	
 	
-	public List<QBaseMSGMessage> getMsgMessageData() {
+	/*public List<QBaseMSGMessage> getMsgMessageData() {
 		return msgMessageData;
 	}
 
 	public void setMsgMessageData(List<QBaseMSGMessage> msgMessageData) {
 		this.msgMessageData = msgMessageData;
+	}*/
+
+	public String getTemplate_code() {
+		return template_code;
 	}
+
+
+	public void setTemplate_code(String template_code) {
+		this.template_code = template_code;
+	}
+
+	public String[] getMsgMessageData() {
+		return msgMessageData;
+	}
+
+	public void setMsgMessageData(String[] msgMessageData) {
+		this.msgMessageData = msgMessageData;
+	}
+
+	public QBaseMSGMessageType getMsgMessageType() {
+		return msgMessageType;
+	}
+
+	public void setMsgMessageType(QBaseMSGMessageType msgMessageType) {
+		this.msgMessageType = msgMessageType;
+	}
+
+	public static String getEventType() {
+		return event_type;
+	}
+	
+	public String[] getAttachments() {
+		return attachments;
+	}
+
+	public void setAttachments(String[] attachments) {
+		this.attachments = attachments;
+	}
+
+
 
 	public QMSGMessage(String msg_type) {
 		super(MESSAGE_TYPE);
 		//this.msgMessageData = new QBaseMSGMessage();
 	}
 
+
 	@Override
 	public String toString() {
-		return "QMSGMessage [msgMessageData=" + msgMessageData + "]";
+		return "QMSGMessage [template_code=" + template_code + ", msgMessageData=" + Arrays.toString(msgMessageData)
+				+ ", msgMessageType=" + msgMessageType + "]";
 	}
 	
 	

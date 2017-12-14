46ab5fa6290debbb0afd211f07152d5946e95335
Anish Maharjan <anish.maharjan12341@gmail.com>
2017-12-14 16:58:35 +1100

    Social Messages

---
+++ src/main/java/life/genny/qwanda/message/QCmdSocialMessage.java
@@ -0,0 +1,28 @@
+// package life.genny.qwanda.message;
+
+// public class QCmdSocialMessage extends QCmdMessage{
+// 	private static final String CMD_TYPE = "SOCIAL_MEDIA_FB_FETCH";
+// 	private static final String CODE = "FB_CODE";
+
+// 	private String fb_code = "";
+	
+// 	public QCmdSocialMessage(String fbCode) {
+// 		super(CMD_TYPE, CODE);
+// 		this.fb_code = fbCode;
+// 		// TODO Auto-generated constructor stub
+// 	}
+
+// 	/**
+// 	 * 
+// 	 */
+// 	private static final long serialVersionUID = 1L;
+
+// 	public String getFb_code() {
+// 		return fb_code;
+// 	}
+
+// 	public void setFb_code(String fbCode) {
+// 		this.fb_code = fbCode;
+// 	}
+	
+// }

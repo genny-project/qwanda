2b4c34e5ce5c01763538c4836b2d35b8874b9592
Adam Crow <acrow@Adams-Compooter.local>
2018-02-08 14:22:47 +1100

    removed detached cascade

--- src/main/java/life/genny/qwanda/entity/EntityEntityId.java
+++ src/main/java/life/genny/qwanda/entity/EntityEntityId.java
@@ -20,7 +20,7 @@ public class EntityEntityId implements java.io.Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 
-	@ManyToOne(fetch = FetchType.EAGER, optional = false, cascade = CascadeType.DETACH)
+	@ManyToOne(fetch = FetchType.EAGER, optional = false)
 	@JsonBackReference(value="entityEntity")
 	private BaseEntity source;
 	

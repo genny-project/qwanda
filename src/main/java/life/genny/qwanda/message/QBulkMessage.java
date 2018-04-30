a0e3ba7ddbc1cabffdd26f7daf5455510e4bf720
Adam Crow <acrow@Adams-Compooter.local>
2018-04-30 10:40:19 +1000

    Updated QBulkMessage

--- src/main/java/life/genny/qwanda/message/QBulkMessage.java
+++ src/main/java/life/genny/qwanda/message/QBulkMessage.java
@@ -11,32 +11,42 @@ public class QBulkMessage implements Serializable {
 	  @Expose
 	  private String token;
 	  
+	  @Expose
 	  private String data_type=	 QBulkMessage.class.getSimpleName();
 
 	  
 	  @Expose
-	  private QMessage[] messages;
+	  private QDataBaseEntityMessage[] messages;
+	  
+	  @Expose
+	  private String[] recipientCodeArray;
+
 	
 	private QBulkMessage(){}
-	public QBulkMessage(QMessage[] qMessages){
+	public QBulkMessage(QDataBaseEntityMessage[] qMessages){
 		this.messages = qMessages;
 	}
 	
-	public QBulkMessage(List<QMessage> qMessages){
-		this.messages = new QMessage[qMessages.size()];
+	public QBulkMessage(QDataBaseEntityMessage qMessage){
+		messages = new QDataBaseEntityMessage[1];
+		this.messages[0] = qMessage;
+	}
+	
+	public QBulkMessage(List<QDataBaseEntityMessage> qMessages){
+		this.messages = new QDataBaseEntityMessage[qMessages.size()];
 		this.messages = qMessages.toArray(this.messages);
 	}
 
 	/**
 	 * @return the messages
 	 */
-	public QMessage[] getMessages() {
+	public QDataBaseEntityMessage[] getMessages() {
 		return messages;
 	}
 	/**
 	 * @param messages the messages to set
 	 */
-	public void setMessages(QMessage[] messages) {
+	public void setMessages(QDataBaseEntityMessage[] messages) {
 		this.messages = messages;
 	}
 	/**
@@ -62,6 +72,18 @@ public class QBulkMessage implements Serializable {
 	 */
 	public void setData_type(String data_type) {
 		this.data_type = data_type;
+	}
+	/**
+	 * @return the recipientCodeArray
+	 */
+	public String[] getRecipientCodeArray() {
+		return recipientCodeArray;
+	}
+	/**
+	 * @param recipientCodeArray the recipientCodeArray to set
+	 */
+	public void setRecipientCodeArray(String[] recipientCodeArray) {
+		this.recipientCodeArray = recipientCodeArray;
 	}
 	
 	

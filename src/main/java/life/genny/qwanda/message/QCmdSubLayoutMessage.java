98febfc3be4fcf7f5a8ea708a8d97b576772cdef
rpgayatri <rpgayatri@gmail.com>
2018-08-17 17:52:11 +1000

    class for sublayout

--- src/main/java/life/genny/qwanda/message/QCmdSubLayoutMessage.java
+++ src/main/java/life/genny/qwanda/message/QCmdSubLayoutMessage.java
@@ -1,55 +1,108 @@
 package life.genny.qwanda.message;
 
-import com.google.gson.JsonObject;
 import com.google.gson.annotations.Expose;
 
 public class QCmdSubLayoutMessage extends QCmdMessage {
-  private static final String CMD_TYPE = "CMD_SUBLAYOUT";
-  @Expose
-  private String data;
-  @Expose
-  private Boolean visible;
 
+	/**
+	   * 
+	   */
+	private static final long serialVersionUID = 1L;
 
+	private static final String CMD_SUBLAYOUT = "CMD_SUBLAYOUT";
+	private static final String CMD_POPUP = "CMD_POPUP";
+	/*
+	 * @Expose private String data;
+	 * 
+	 * @Expose private Boolean visible;
+	 */
 
-  public QCmdSubLayoutMessage(final String subLayoutCode, final String subLayout) {
-    super(CMD_TYPE, subLayoutCode);
-    setData(subLayout);
-  }
+	@Expose
+	private String layoutCode;
 
-  /**
-   * 
-   */
-  private static final long serialVersionUID = 1L;
+	@Expose
+	private String root;
 
-  /**
-   * @return the data
-   */
-  public String getData() {
-    return data;
-  }
+	@Expose
+	private String items;
+	
+	@Expose
+	private Boolean isPopup;
 
-  /**
-   * @param data the data to set
-   */
-  private void setData(final String data) {
-    this.data = data;
-  }
+	public QCmdSubLayoutMessage(final String layoutCode, Boolean isPopup) {
+		super(isPopup ? CMD_POPUP : CMD_SUBLAYOUT, layoutCode);
+	}
+	
+	public QCmdSubLayoutMessage(final String layoutCode, String items, Boolean isPopup, String root) {
+		super(isPopup ? CMD_POPUP : CMD_SUBLAYOUT, layoutCode);
+		setItems(items);
+		setRoot(root);
+	}
 
-  /**
-   * @return the visible
-   */
-  public Boolean getVisible() {
-    return visible;
-  }
+	/**
+	 * @return the layoutCode
+	 */
+	public String getLayoutCode() {
+		return layoutCode;
+	}
 
-  /**
-   * @param visible the visible to set
-   */
-  public void setVisible(final Boolean visible) {
-    this.visible = visible;
-  }
+	/**
+	 * @param layoutCode the layoutCode to set
+	 */
+	public void setLayoutCode(String layoutCode) {
+		this.layoutCode = layoutCode;
+	}
 
+	/**
+	 * @return the root
+	 */
+	public String getRoot() {
+		return root;
+	}
+
+	/**
+	 * @param root the root to set
+	 */
+	public void setRoot(String root) {
+		this.root = root;
+	}
+
+	/**
+	 * @return the items
+	 */
+	public String getItems() {
+		return items;
+	}
+
+	/**
+	 * @param items the items to set
+	 */
+	public void setItems(String items) {
+		this.items = items;
+	}
+
+	/**
+	 * @return the isPopup
+	 */
+	public Boolean getIsPopup() {
+		return isPopup;
+	}
+
+	/**
+	 * @param isPopup the isPopup to set
+	 */
+	public void setIsPopup(Boolean isPopup) {
+		this.isPopup = isPopup;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "QCmdSubLayoutMessage [layoutCode=" + layoutCode + ", root=" + root + ", items=" + items + ", isPopup="
+				+ isPopup + "]";
+	}
 
 
 }

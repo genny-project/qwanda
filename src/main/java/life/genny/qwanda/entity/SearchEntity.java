60bb4d1a045ba498ad57418389d532442fc3470c
Sudan <sudansth@gmail.com>
2018-10-04 10:44:00 +1000

    Documenting the SearchEntity

--- src/main/java/life/genny/qwanda/entity/SearchEntity.java
+++ src/main/java/life/genny/qwanda/entity/SearchEntity.java
@@ -11,6 +11,9 @@ import life.genny.qwanda.attribute.AttributeText;
 import life.genny.qwanda.exception.BadDataException;
 import life.genny.qwandautils.JsonUtils;
 
+/* SearchEntity class implements the search of base entities applying different filters/search to the
+ * baseEntity and its attributes 
+ */
 public class SearchEntity extends BaseEntity {
 	/**
 	 * 
@@ -20,6 +23,10 @@ public class SearchEntity extends BaseEntity {
 	Double colIndex = 1.0;
 	Double sortIndex = 1.0;
 	
+	/*
+	 * This Sort Enum is used to sort the search results in 
+	 * either Ascending and descending order
+	 */
 	public enum Sort {
 	    ASC {
 	        public String toString() {
@@ -33,6 +40,10 @@ public class SearchEntity extends BaseEntity {
 	    }
 	}
 
+	/*
+	 * This StringFilter Enum is used to put the filter to the search entity. 
+	 * It filters the string values in the attributes
+	 */
 	public enum StringFilter {
 	    EQUAL {
 	        public String toString() {
@@ -45,6 +56,11 @@ public class SearchEntity extends BaseEntity {
 	        }
 	    }
 	}
+	
+	/*
+	 * This Filter Enum is used to put the filter to the search entity. 
+	 * It filtesr the numeric and bit masked values of the attributes
+	 */
 	public enum Filter {
 	    EQUALS {
 	        public String toString() {
@@ -83,13 +99,14 @@ public class SearchEntity extends BaseEntity {
        }
 	}
 	
-
+	/* Constructor to create SearchEntity with code and name */
 	public SearchEntity(final String code, final String name) {
 		super(code,name);
 		setPageStart(0);
 		setPageSize(20);
 	}
 	
+	/* Constructor to create SearchEntity passing BaseEntity */
 	public SearchEntity(final BaseEntity be) {
 		super(be.getCode(), be.getName());
 		this.setCreated(be.getCreated());
@@ -97,6 +114,10 @@ public class SearchEntity extends BaseEntity {
 		this.setBaseEntityAttributes(be.getBaseEntityAttributes());
 	}
 	
+	/*
+	 * This method allows to add the attributes to the SearchEntity that is required in the result 
+	 * BaseEntities
+	 */
 	public SearchEntity addColumn(final String attributeCode, final String columnName)
 	{
 		AttributeText attributeColumn = new AttributeText("COL_"+attributeCode, columnName);
@@ -109,6 +130,10 @@ public class SearchEntity extends BaseEntity {
 		return this;
 	}
 	
+	/*
+	 * This method allows to add sorting to the attributes of the search results
+	 * It can either sort in ascending or descending order
+	 */
 	public SearchEntity addSort(final String attributeCode, final String sortHelpText, final Sort sortType)
 	{
 		AttributeText attributeSort = new AttributeText("SRT_"+attributeCode, sortHelpText);
@@ -123,6 +148,12 @@ public class SearchEntity extends BaseEntity {
 		return this;
 	}
 	
+	/*
+	 * This method allows to set the filter for the integer value in the search 
+	 * @param attributeCode - the attributeCode which holds integer value where we apply the filter
+	 * @param filterType - type of the filter
+	 * @param value - filter against (search for)  this value
+	 */
 	public SearchEntity addFilter(final String attributeCode, final Filter filterType, final Integer value)
 	{
 		AttributeInteger attribute = new AttributeInteger(attributeCode, filterType.toString());
@@ -135,6 +166,12 @@ public class SearchEntity extends BaseEntity {
 		return this;
 	}
 
+	/*
+	 * This method allows to set the filter for the Long value in the search 
+	 * @param attributeCode - the attributeCode which holds long value where we apply the filter
+	 * @param filterType - type of the filter
+	 * @param value - filter against (search for)  this value
+	 */
 	public SearchEntity addFilter(final String attributeCode, final Filter filterType, final Long value)
 	{
 		AttributeLong attribute = new AttributeLong(attributeCode, filterType.toString());
@@ -147,6 +184,12 @@ public class SearchEntity extends BaseEntity {
 		return this;
 	}
 	
+	/*
+	 * This method allows to set the filter for the LocalDateTime value in the search 
+	 * @param attributeCode - the attributeCode which holds LocalDateTime value where we apply the filter
+	 * @param filterType - type of the filter
+	 * @param value - filter against (search for)  this value
+	 */
 	public SearchEntity addFilter(final String attributeCode, final Filter filterType, final LocalDateTime value)
 	{
 		AttributeDateTime attribute = new AttributeDateTime(attributeCode, filterType.toString());
@@ -159,7 +202,11 @@ public class SearchEntity extends BaseEntity {
 		return this;
 	}	
 	
-	
+	/*
+	 * This method allows to set the filter for the Boolean value in the search 
+	 * @param attributeCode - the attributeCode which holds Boolean value where we apply the filter
+	 * @param value - filter against (search for) this value
+	 */
 	public SearchEntity addFilter(final String attributeCode, final Boolean value)
 	{
 		AttributeBoolean attribute = new AttributeBoolean(attributeCode, "=");
@@ -172,6 +219,12 @@ public class SearchEntity extends BaseEntity {
 		return this;
 	}	
 
+	/*
+	 * This method allows to set the filter for the String value in the search 
+	 * @param attributeCode - the attributeCode which holds String value where we apply the filter
+	 * @param filterType - type of the string filter
+	 * @param value - filter against (search for) this value
+	 */
 	public SearchEntity addFilter(final String attributeCode, final StringFilter filterType,final String value)
 	{
 		AttributeText attribute = new AttributeText(attributeCode, filterType.toString());
@@ -184,6 +237,11 @@ public class SearchEntity extends BaseEntity {
 		return this;
 	}	
 	
+	/*
+	 * This method allows to set the LinkWeight to the resulted BaseEntities 
+	 * to its parent 
+	 * @param value - value/linkWeight to be set
+	 */
 	public SearchEntity setLinkWeight(final Double value) 
 	{
 		AttributeDouble attribute = new AttributeDouble("SCH_LINK_WEIGHT", "LinkWeight");
@@ -196,6 +254,10 @@ public class SearchEntity extends BaseEntity {
 		return this;
 	}
 	
+	/*
+	 * This method allows to set the filter based on the linkweight value of BaseEntities to its parent
+	 * @param filterType - type of the filter set to the linkWeight
+	 */
 	public SearchEntity addFilterToLinkWeight(final Filter filterType)
 	{
 		AttributeText attribute = new AttributeText("SCH_LINK_FILTER", "LinkFilterByWeight");
@@ -208,6 +270,10 @@ public class SearchEntity extends BaseEntity {
 		return this;
 	}
 	
+	/*
+	 * This method allows to set the start/begining number of the range(page) of the results data to be sent
+	 * @param pageStart - start of the page number
+	 */
 	public SearchEntity setPageStart(final Integer pageStart)
 	{
 		AttributeInteger attributePageStart = new AttributeInteger("SCH_PAGE_START", "PageStart");
@@ -220,6 +286,10 @@ public class SearchEntity extends BaseEntity {
 		return this;
 	}
 	
+	/*
+	 * This method allows to set the total number of the results (BaseEntites) to be sent
+	 * @param pageSize - total number of data's to be sent
+	 */
 	public SearchEntity setPageSize(final Integer pageSize)
 	{
 		AttributeInteger attributePageSize = new AttributeInteger("SCH_PAGE_SIZE", "PageSize");
@@ -232,6 +302,11 @@ public class SearchEntity extends BaseEntity {
 		return this;
 	}
 
+	/*
+	 * This method allows to set the stakeholder/user code to the search. It will search for the BaseEntites 
+	 * that the given user is stakeholder of.
+	 * @param stakeholderCode - userCode
+	 */
 	public SearchEntity setStakeholder(final String stakeholderCode)
 	{
 		AttributeText attribute = new AttributeText("SCH_STAKEHOLDER_CODE", "Stakeholder");
@@ -245,7 +320,9 @@ public class SearchEntity extends BaseEntity {
 	}
 	
 	/*
-	 * Setting the stakeholder code of the Source Baseentity
+	 * This method allows to set the stakeholder/user code to the parent/source Basentity involved in the search. 
+	 * It will search for the BaseEntites under the give source BE that the given user is stakeholder of.
+	 * @param sourceStakeholderCode - userCode
 	 */
 	public SearchEntity setSourceStakeholder(final String sourceStakeholderCode)
 	{
@@ -259,6 +336,11 @@ public class SearchEntity extends BaseEntity {
 		return this;
 	}
 	
+	/*
+	 * This method allows to set the stakeholder/user code to the parent/source Basentity involved in the search. 
+	 * It will search for the BaseEntites under the give source BE that the given user is stakeholder of.
+	 * @param sourceStakeholderCode - userCode
+	 */
 	public SearchEntity setLinkCode(final String linkCode)
 	{
 		AttributeText attribute = new AttributeText("SCH_LINK_CODE", "LinkCode");
@@ -271,6 +353,10 @@ public class SearchEntity extends BaseEntity {
 		return this;
 	}
 	
+	/*
+	 * This method allows to set the link value the result of the search. 
+	 * @param linkValue - linkValue of the sourceCode to the results (BaseEntities) of the search
+	 */
 	public SearchEntity setLinkValue(final String linkValue)
 	{
 		AttributeText attribute = new AttributeText("SCH_LINK_VALUE", "LinkValue");

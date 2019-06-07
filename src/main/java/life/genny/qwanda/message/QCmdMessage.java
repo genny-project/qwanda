58b59cb78d9b9a312086d244b2b53bb31050dbae
ooanishoo <anish.maharjan12341@gmail.com>
2019-06-07 14:57:30 +1000

    exposed contextList

--- src/main/java/life/genny/qwanda/message/QCmdMessage.java
+++ src/main/java/life/genny/qwanda/message/QCmdMessage.java
@@ -1,22 +1,34 @@
 package life.genny.qwanda.message;
 
-import java.io.Serializable;
-
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.google.gson.annotations.Expose;
 
-public class QCmdMessage extends QMessage{
+import javax.persistence.Embedded;
+import javax.validation.Valid;
+
+import life.genny.qwanda.ContextList;
+
+public class QCmdMessage extends QMessage {
 
 	private static final String MESSAGE_TYPE = "CMD_MSG";
 	@Expose
 	private String cmd_type;
 	@Expose
 	private String code;
-	
-	public QCmdMessage(String cmd_type, String code){
+
+	@Embedded
+	@Valid
+	@JsonInclude(Include.NON_NULL)
+	@Expose
+	private ContextList contextList;
+
+	public QCmdMessage(String cmd_type, String code) {
 		super(MESSAGE_TYPE);
 		this.code = code;
 		this.cmd_type = cmd_type;
 	}
+
 	public String getCmd_type() {
 		return cmd_type;
 	}
@@ -25,13 +37,26 @@ public class QCmdMessage extends QMessage{
 		this.cmd_type = cmd_type;
 	}
 
-
 	public String getCode() {
 		return code;
 	}
 
 	public void setCode(String code) {
 		this.code = code;
+	}
+
+	/**
+	 * @return the contextList
+	 */
+	public ContextList getContextList() {
+		return contextList;
+	}
+
+	/**
+	 * @param contextList the contextList to set
+	 */
+	public void setContextList(final ContextList contextList) {
+		this.contextList = contextList;
 	}
 
 }

d05198bf578577a62d89d2767c6c1d8bf81e975e
Adam Crow <adamcrow63@gmail.com>
2020-07-03 14:17:19 +1000

    Added wildcard too searchEntty

--- src/main/java/life/genny/qwanda/entity/SearchEntity.java
+++ src/main/java/life/genny/qwanda/entity/SearchEntity.java
@@ -451,7 +451,24 @@ public class SearchEntity extends BaseEntity {
 	}
 
 	
-	
+	/*
+	 * This method allows to set thewildcard of the results data to be sent
+	 * @param wildcard - the widlcard
+	 */
+	public SearchEntity setWildcard(String wildcard)
+	{
+		// only allow characters
+		wildcard = wildcard.replaceAll(("[^A-Za-z0-9 ]"), "");
+		
+		AttributeText attributeWildcard = new AttributeText("SCH_WILDCARD", "Wildcard");
+		try {
+			addAttribute(attributeWildcard, 1.0, wildcard);
+		} catch (BadDataException e) {
+			log.error("Bad Wildcard ");
+		}
+
+		return this;
+	}
 	
 	/* (non-Javadoc)
 	 * @see java.lang.Object#toString()

7eb7f4b58348fd87f243f806490f0fbd6d0d7f61
Adam Crow <adamcrow63@gmail.com>
2020-07-29 18:55:34 +1000

    Updated QDataAsk and QDataBaseEntity null items

--- src/main/java/life/genny/qwanda/message/QDataAskMessage.java
+++ src/main/java/life/genny/qwanda/message/QDataAskMessage.java
@@ -5,6 +5,7 @@ import java.io.Serializable;
 import com.google.gson.annotations.Expose;
 
 import life.genny.qwanda.Ask;
+import life.genny.qwanda.entity.BaseEntity;
 
 public class QDataAskMessage extends QDataMessage implements Serializable {
 
@@ -16,9 +17,14 @@ public class QDataAskMessage extends QDataMessage implements Serializable {
 	private Ask[] items;
 	private static final String DATATYPE_ASK = Ask.class.getSimpleName();
 
-	public QDataAskMessage(Ask[] asks) {
+	public QDataAskMessage(Ask[] items) {
 		super(DATATYPE_ASK);
-		setItems(asks);
+		if ((items == null)||(items.length == 0)) {
+			setItems(new Ask[0]);
+		} else {
+			setItems(items);
+		}
+
 	}
 	
 	public QDataAskMessage(Ask ask) {
@@ -33,7 +39,12 @@ public class QDataAskMessage extends QDataMessage implements Serializable {
 	}
 
 	public void setItems(Ask[] asks) {
-		this.items = asks;
+		if ((items == null)||(items.length == 0)) {
+			this.items = new Ask[0];
+		} else {
+			this.items = asks;
+		}
+
 	}
 
 }

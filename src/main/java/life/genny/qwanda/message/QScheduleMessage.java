f58a0eae020459953cfbe81601ea831f605dca1f
Adam Crow <adamcrow63@gmail.com>
2020-10-27 10:52:08 +1100

    Added an absolute trigger timedate for shleemy scheduling

--- src/main/java/life/genny/qwanda/message/QScheduleMessage.java
+++ src/main/java/life/genny/qwanda/message/QScheduleMessage.java
@@ -1,5 +1,6 @@
 package life.genny.qwanda.message;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 
@@ -7,14 +8,22 @@ import java.time.ZoneId;
 import com.google.gson.annotations.Expose;
 
 
-public class QScheduleMessage {
+public class QScheduleMessage implements Serializable {
 
+	
+		/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	
 		public LocalDateTime created = LocalDateTime.now(ZoneId.of("UTC"));
 		public LocalDateTime updated;
 
 		@Expose
 		public String cron;
+		
+		@Expose
+		public LocalDateTime triggertime;
 		
 		@Expose
 		public String realm;
@@ -36,6 +45,14 @@ public class QScheduleMessage {
 	public QScheduleMessage(final String jsonMessage, final String sourceCode, final String channel, final String cron, final String realm)
 	{
 		this.cron = cron;
+		this.jsonMessage = jsonMessage;
+		this.channel = channel;
+		this.sourceCode = sourceCode;
+	}
+	
+	public QScheduleMessage(final String jsonMessage, final String sourceCode, final String channel, final LocalDateTime triggertime, final String realm)
+	{
+		this.triggertime = triggertime;
 		this.jsonMessage = jsonMessage;
 		this.channel = channel;
 		this.sourceCode = sourceCode;

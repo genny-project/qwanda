1e7653d24b33fa8a918824692d1c1a9c9794e711
Adam Crow <adamcrow63@gmail.com>
2020-11-10 10:12:53 +1100

    Added QDropdownMessage

--- src/test/java/life/genny/test/QBulkPullMessageTest.java
+++ src/test/java/life/genny/test/QBulkPullMessageTest.java
@@ -12,6 +12,7 @@ import com.google.gson.GsonBuilder;
 
 import life.genny.qwanda.MoneyDeserializer;
 import life.genny.qwanda.message.QBulkPullMessage;
+import life.genny.qwanda.message.QEventDropdownMessage;
 
 public class QBulkPullMessageTest {
 	
@@ -22,6 +23,29 @@ public class QBulkPullMessageTest {
 			.getLogger(MethodHandles.lookup().lookupClass().getCanonicalName());
 
 
+	@Test
+	public void ddEventTest()
+	{
+		QEventDropdownMessage msg = new QEventDropdownMessage("LNK_HOST_COMPANY");
+		msg.getData().setTargetCode("PER_YTRYTRYTRYTRYTR");
+		msg.setSourceCode("PER_757657657657657");
+		msg.setPageIndex(0);
+		msg.setPageSize(20);
+		
+		GsonBuilder gsonBuilder = new GsonBuilder().registerTypeAdapter(Money.class, new MoneyDeserializer());
+		Gson gson = gsonBuilder.create();
+
+		String moneyJson2 = gson.toJson(msg);
+		System.out.println(moneyJson2);
+		
+		msg.data.setValue("GAD");
+		
+		moneyJson2 = gson.toJson(msg);
+		System.out.println(moneyJson2);
+
+	}
+
+	
 @Test
 public void jsonTest()
 {

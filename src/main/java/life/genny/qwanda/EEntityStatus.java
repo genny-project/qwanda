269cedc9bb81140abacda6b35572c2d9347f1bc4
Jasper Robison <jasper.robison22@gmail.com>
2021-07-27 10:07:00 +1000

    add more status'

--- src/main/java/life/genny/qwanda/EEntityStatus.java
+++ src/main/java/life/genny/qwanda/EEntityStatus.java
@@ -5,10 +5,13 @@ public enum EEntityStatus {
 	// This enum is designed for simplicity and speed. 
 	// The database will be expected to default to ACTIVE initially and we'll slowly bring in theother statuses
 
-	ACTIVE,      // 0 - This status means that the entity should be picked up in all database fetches
-	PENDING,     // 1 - This means that the entity is currently under construction and cannot be fetched under normal operation
-	DELETED,     // 2 - This means that the entity is marked for deleted
-	ARCHIVED,    // 3 - This means that the entity is not to be deleted , but is archived but not visible to normal operation
-	DISABLED     // 4 - This means that the entity is currently not to be fetched, but is expected to possibly be returned to active status
+	ACTIVE,          // 0 - This status means that the entity should be picked up in all database fetches
+	TEST,            // 1 - This means that the entity is a well crafted test entity for use by devs
+	TEMPORARY,       // 3 - This means that the entity is a useful and persistent test object
+	PENDING,         // 4 - This means that the entity is currently under construction and cannot be fetched under normal operation
+	DISABLED,        // 5 - This means that the entity is currently not to be fetched, but is expected to possibly be returned to active status
+	ARCHIVED,        // 6 - This means that the entity is not to be deleted , but is archived but not visible to normal operation
+	PENDING_DELETE,  // 7 - This means that the entity is pending deletion, but not yet deleted
+	DELETED,         // 8 - This means that the entity is marked deleted
 	
 }

86c4f03ecb12057039d03d17fbe04c084be3ce3a
Jasper Robison <jasper.robison22@gmail.com>
2021-12-02 15:44:33 +1100

    new class file

---
+++ src/main/java/life/genny/qwanda/message/QSearchMessage.java
@@ -0,0 +1,47 @@
+package life.genny.qwanda.message;
+
+import java.io.Serializable;
+
+import com.google.gson.annotations.Expose;
+
+import life.genny.qwanda.entity.SearchEntity;
+
+public class QSearchMessage extends QMessage implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	private static final String MESSAGE_TYPE = "SCH_MSG";
+	@Expose
+	private SearchEntity searchEntity;
+	@Expose
+	private String destination;
+
+	public QSearchMessage() {
+		super(MESSAGE_TYPE);
+	}
+
+	public QSearchMessage(SearchEntity searchEntity) {
+		super(MESSAGE_TYPE);
+		this.searchEntity = searchEntity;
+	}
+
+	@Override
+	public String toString() {
+		return "QSearchMessage [searchEntity=" + searchEntity.getCode() + "]";
+	}
+
+	public void setSearchEntity(SearchEntity searchEntity) {
+		this.searchEntity = searchEntity;
+	}
+
+	public SearchEntity getSearchEntity() {
+		return this.searchEntity;
+	}
+	
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+
+	public String getDestination() {
+		return this.destination;
+	}
+}

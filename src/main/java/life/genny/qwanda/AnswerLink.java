f01aac5882231e82a215d14e88afe4fce89e88b3
Expressionless <rhysmchaen@gmail.com>
2022-04-16 12:33:58 +1000

    Updated answer null message

--- src/main/java/life/genny/qwanda/AnswerLink.java
+++ src/main/java/life/genny/qwanda/AnswerLink.java
@@ -227,6 +227,7 @@ public class AnswerLink implements java.io.Serializable {
 
 		if (this.getAttribute() == null) {
 			log.error("attribute is NULl in setAnswer");
+			log.error("Attribute code: " + answer.getAttributeCode());
 		} else {
 			if (this.getAttribute().getDataType() == null) {
 				log.error("attribute " + this.getAttribute().getCode() + " -> DATA_TYPE is NULl in setAnswer");

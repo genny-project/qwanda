7db64abfa98358dae53e00ab229d103fcb6ae9e7
Expressionless <rhysmchaen@gmail.com>
2022-05-04 12:04:03 +1000

    Reverting qwanda:10.0.0 to last commit from HEAD at 2022-04-28 22:00. Hash: 6d8dd4bb44eeb3ac893b7983eca5dbcde0796d5a

--- src/main/java/life/genny/qwanda/utils/CommonUtils.java
+++ src/main/java/life/genny/qwanda/utils/CommonUtils.java
@@ -22,8 +22,8 @@ public class CommonUtils {
      * @param env - Environment variable to fetch
      * @return - value of env or null if it is missing
      */
-    public static String getSystemEnv(String env) {
-        return getSystemEnv(env, true);
+    public static String getEnv(String env) {
+        return getEnv(env, true);
     }
 
     /**
@@ -32,7 +32,7 @@ public class CommonUtils {
      * @param alert whether or not to log if the environment variable is missing (default true)
      * @return - value of env or null if it is missing
      */
-    public static String getSystemEnv(String env, boolean alert) {
+    public static String getEnv(String env, boolean alert) {
         String result = System.getenv(env);
         if(result == null && alert) {
             log.error("Cannot get environment variable " + env + ". Is it set?");
